da9ce5270c2eb1b5de78e62b54aa4ae3a175fcd9
Morten40300 <64534853+Morten40300@users.noreply.github.com>
2020-05-03 17:32:36 +0200

    Update main.php

--- main.php
+++ main.php
@@ -32,7 +32,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Tjekker forbindelsen
 if ($conn->connect_error) { 
-    echo("Der kunne ikke forbindes til serveren!");
+    echo("Der kunne ikke forbindes til databasen!");
 } 
 
 //Her indgår der MySQLi sprog, som fortæller: Hent data fra kolonnerne "placering", "temp" og "fugt" fra tabellen "SensorData", hvor den kun skal tag data fra rækken, hvis kolonne-værdien er "Indenfor". Derudover sortere den dataen efter den nyeste data (dvs. "seneste_daata).
@@ -43,15 +43,17 @@ if ($conn->connect_error) {
 $sql = "SELECT placering, temp, fugt FROM SensorData WHERE placering = 'Udenfor' ORDER BY seneste_data DESC LIMIT 1";
 
 
-//Opretter en forspørgelse til databasen med kriterierne vi lavede ved "$sql", og outputter rækkerne til variablen "$result".
+//Opretter en forspørgelse til databasen med kriterierne vi lavede ved "$sql", og outputter data'en til variablen "$result".
 if ($result = $conn->query($sql)) {
-    //Tjekker hvis der er flere end 0 rækker med data, så vil funktionen "fetch_assoc()" skabe et array med al dataen, som vi looper igennem med while'en. While'en køre igennem arrayet med dataen, hvor der inde i while'en er variabler det blive sat lige med værdierne i arrayet.
+    
+    //Opret array med hentet data
+    //Sæt variabler lig med værdierne i arrayet
     while ($row = $result->fetch_assoc()) {
-        $placering_1 = $row["placering"]; //Sætter variabel lig med værdien i kolonnnen "placering" i den seneste række med data. 
-        $temperatur_1 = $row["temp"]; //Sætter variabel lig med værdien i kolonnnen "temp" i den seneste række med data.
-        $fugtighed_1 = $row["fugt"]; //Sætter variabel lig med værdien i kolonnnen "fugt" i den seneste række med data.
+        $placering_1 = $row["placering"]; //Værdi i kolonne ”placering”
+        $temperatur_1 = $row["temp"]; //Værdi i kolonne ”temp”
+        $fugtighed_1 = $row["fugt"]; //Værdi i kolonne ”fugt”
     }
-    $result->free(); //Sletter den gemte data, som variablen $result har.
+    $result->free(); //Sletter den hentede data på $result.
 }
 
 
